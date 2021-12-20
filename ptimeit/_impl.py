3b0982515d4e9a8b7f2ad5fa736a70b687c688a2
Karlo Kotrba <karlo@constructor.io>
2021-12-20 20:55:56 +0100

    addressed comments

--- ptimeit/_impl.py
+++ ptimeit/_impl.py
@@ -10,14 +10,14 @@ _depth = -1
 @contextmanager
 def timeit_section(
     section_name: str,
-    should_print: Callable[[float], bool] = lambda t: True,
+    condition: Callable[[float], bool] = None,
     additional_data_to_print: str = None
 ):
     """
     Returns a context manager for profiling code section execution time.
 
     :param section_name: section name to use in the output record.
-    :param should_print: a function to determine whether to print the timings.
+    :param condition: a function to determine whether to print the timings.
     :param additional_data_to_print: allows for passing in the additional data
                                      to print with the timings.
 
@@ -42,29 +42,29 @@ def timeit_section(
     finally:
         elapsed_time_s = time.monotonic() - start_time
         elapsed_time_ms = round(float(elapsed_time_s * 1000), 1)
-        if should_print is None or should_print(elapsed_time_ms):
+        if condition is None or condition(elapsed_time_ms):
             elapsed_time_ms_str = str(elapsed_time_ms).rjust(14)
             offset = '|   ' * _depth
             section_full_name = offset + section_name
-            print(f'->>>>>>>>{elapsed_time_ms_str}ms      {section_full_name}')
+            to_print=f'->>>>>>>>{elapsed_time_ms_str}ms      {section_full_name}'
             if additional_data_to_print:
-                print(f'->>>>>>>>>>>>>>>>>>>>>>>>      {offset}'
-                f'Additional data: {additional_data_to_print}'
-            )
+                to_print += f'- {additional_data_to_print}'
+
+            print(to_print)
 
         _depth -= 1
 
 
 def timeit_function(
     function_name: str,
-    should_print: Callable[[float], bool] = lambda t: True,
+    condition: Callable[[float], bool] = None,
     additional_data_to_print: str = None
 ):
     """
     Returns a function decorator for profiling code section execution time.
 
     :param function_name: function name to use in the output record.
-    :param should_print: a function to determine whether to print the timings.
+    :param condition: a function to determine whether to print the timings.
     :param additional_data_to_print: allows for passing in the additional data
                                      to print with the timings.
 
@@ -88,7 +88,7 @@ def timeit_function(
     def decorator(func):
         @functools.wraps(func)
         def timeit_wrapper(*args, **kwargs):
-            with timeit_section(f'{function_name}()', should_print, additional_data_to_print):
+            with timeit_section(f'{function_name}()', condition, additional_data_to_print):
                 return func(*args, **kwargs)
 
         return timeit_wrapper
